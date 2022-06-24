668b5d924fb07aa190f213081dfe16eb876605eb
Avery Yang <averyyang2674@gmail.com>
2022-06-24 15:55:50 -0500

    server.js

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// Require express - gives us a function
+const express = require('express');
+
+// Create an instance of express by calling the function returned above - gives us an object
+const app = express();
+const port = 5000;
+
+// express static file serving - public is the folder name
+app.use(express.static('server/public'));
+
+//middlware
+app.use(bodyParser.urlencoded({ extended: true });
+
+// Start up our server
+app.listen(port, () => {
+  console.log('listening on port', port);
+});
