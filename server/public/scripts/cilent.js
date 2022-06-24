5ca2f8432ef7ab42397fa1815193d7578024dc50
Avery Yang <averyyang2674@gmail.com>
2022-06-24 18:17:52 -0500

    add grabInputs and getResults function

--- server/public/scripts/cilent.js
+++ server/public/scripts/cilent.js
@@ -6,9 +6,64 @@ function ready() {
     console.log(`I'm ready!`);
     //clear inputs
     $('#clear').on('click', clearInputs);
+    $('#equal').on('click', grabInputs);
+}
+
+function grabInputs() {
+    //declare num1 and num2 variables
+    let num1 = $('#num1').val();
+    let num2 = $('#num2').val();
+    // put into object
+    const operands = {
+        num1: Number(num1),
+        num2: Number(num2),
+    };
+    console.log(operands);
+    // POST request to server
+    $.ajax({
+        url: '/home',
+        method: 'POST',
+        data: operands,
+    })
+        .then(function (response) {
+            console.log(response);
+            getResults();
+        })
+        .catch(function (error) {
+            console.log(`ERROR in /POST request: ${error.message}`);
+        });
 }
 
 //clear inputs
 function clearInputs() {
     $('input').val('');
+}
+
+//GET request to server
+function getResults() {
+    // AJAX
+    $.ajax({
+        url: '/home',
+        method: 'GET',
+    })
+        .then(function (response) {
+            console.log(response);
+            // render to DOM
+            render(response);
+        })
+        .catch(function (error) {
+            //404, 500, etc
+            console.log(error);
+            alert('ERROR IN GET /home');
+        });
+}
+
+//display to DOM
+function render(array) {
+    //empty and redraw
+    $('ul').empty();
+    //append it to the DOM
+    for (let obj of array) {
+        $('ul').append(`<li>${obj.num1} ${obj.symbol} ${obj.num2} </li>`);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,23 +1,57 @@
 // Require express - gives us a function
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// Create an instance of express by calling the function returned above - gives us an object
 const app = express();
 const port = 5000;
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
 
-//middlware
-// app.use(bodyParser.urlencoded({ extended: true }));
+//require middlware
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Start up our server
-app.listen(port, () => {
-    console.log('listening on port', port);
+//calculations array
+const calcArray = [];
+
+// GET request
+app.get('/home', function (req, res) {
+    console.log('IN /HOME');
+    res.send(calcArray);
+});
+
+// POST response
+app.post('/home', (req, res) => {
+    console.log('POST /home', req.body);
+    calcArray.push(req.body);
+    //check to make sure object is inside
+    console.log(calcArray);
+    // CREATED
+    res.sendStatus(201);
 });
 
 //handle all the operations
-function handleOperations() {}
+function handleOperations(calcArray) {
+    //loop through calcArray
+    for (let i = 0; i < calcArray.length; i++) {
+        //grab the last numbers
+        console.log(calcArray[i]);
+    }
+
+    // //do shit with it
+    // if ($(this).data('id') === 'add') {
+    //     add(num1, num2);
+    // }
+    // if ($(this).data('id') === 'subtract') {
+    //     subtract(num1, num2);
+    // }
+    // if ($(this).data('id') === 'multiply') {
+    //     multiply(num1, num2);
+    // }
+    // if ($(this).data('id') === 'divide') {
+    //     divide(num1, num2);
+    // }
+}
 
 //add
 function add(num1, num2) {
@@ -38,3 +72,8 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+// Start up our server
+app.listen(port, () => {
+    console.log('listening on port', port);
+});
