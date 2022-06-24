cef6e0e8cef306bc0c991e46bb9a3c994e8069d4
Avery Yang <averyyang2674@gmail.com>
2022-06-24 17:04:56 -0500

    add clearInputs function

--- server/public/scripts/cilent.js
+++ server/public/scripts/cilent.js
@@ -4,4 +4,11 @@ $(ready);
 //ready function
 function ready() {
     console.log(`I'm ready!`);
+    //clear inputs
+    $('#clear').on('click', clearInputs);
+}
+
+//clear inputs
+function clearInputs() {
+    $('input').val('');
 }
--- server/server.js
+++ server/server.js
@@ -15,3 +15,26 @@ app.use(express.static('server/public'));
 app.listen(port, () => {
     console.log('listening on port', port);
 });
+
+//handle all the operations
+function handleOperations() {}
+
+//add
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//subtract
+function subtract(num1, num2) {
+    return (num1 -= num2);
+}
+
+//multiply
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+//divide
+function divide(num1, num2) {
+    return num1 / num2;
+}
